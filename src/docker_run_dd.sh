9e80c47ae80e4dc9bbb0254766a91cb517e0a7fd
Dong Du <ddnirvana1@gmail.com>
2021-03-19 12:59:39 +0800

    docker run dd
    Signed-off-by: Dong Du <ddnirvana1@gmail.com>

---
+++ src/docker_run_dd.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --rm -it -p 12302:40041 -v /home/dd/devlop/serverless/molecule-faas/molecule-benchmarks/:/home -v /home/dd/devlop/serverless/molecule-faas/env-javascript/src/:/env --entrypoint=/bin/bash registry.cn-shanghai.aliyuncs.com/jointfaas-serverless/env-javascript:v6.1
