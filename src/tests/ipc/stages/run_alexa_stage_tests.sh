66237c144bb68758de1ba3b1fb2ae7322264229c
Dong Du <ddnirvana1@gmail.com>
2021-04-09 10:20:45 +0800

    add scripts to run local network based dag communication: baseline
    Signed-off-by: Dong Du <ddnirvana1@gmail.com>

---
+++ src/tests/ipc/stages/run_alexa_stage_tests.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function run_test(){
+	#NAME=interact-smarthome
+	#./docker_run-IPC-client.sh tests/ipc/stages/$NAME/
+	./docker_run-IPC-client.sh tests/ipc/stages/$1/
+	sleep 1
+	#./docker_run-IPC-server.sh tests/ipc/stages/$NAME/
+	./docker_run-IPC-server.sh tests/ipc/stages/$1/
+	sleep 1
+
+	./test_ipc.sh
+
+	sleep 2
+
+	#docker logs ipc_stage_test_caller > $NAME-caller_logs.txt
+	#docker logs ipc_stage_test_callee > $NAME-callee_logs.txt
+	docker logs ipc_stage_test_caller > $1-caller_logs.txt
+	docker logs ipc_stage_test_callee > $1-callee_logs.txt
+
+	##Clean
+	docker stop $(docker ps -aq)
+}
+
+##Clean
+docker stop $(docker ps -aq)
+
+# 1. front-end -> interact
+run_test front-interact
+
+# 2. interact-smarthome
+run_test interact-smarthome
+
+# 3. smarthome-door
+run_test smarthome-door
+
+# 4. smarthome-light
+run_test smarthome-light
+
+##Clean
+docker stop $(docker ps -aq)
---
+++ src/tests/ipc/stages/smarthome-light/env_caller.sh
@@ -0,0 +1,5 @@
+export CODE_PATH=/home/src/smarthome/index.js
+export INFO_HOST=172.17.0.1
+export INFO_PORT=12302
+export NEXTSTEPNAME=light
+export FUNC_NAME=smarthome
