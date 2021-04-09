ff149e1cd27448770ef636e967563d54d5d74c16
Dong Du <ddnirvana1@gmail.com>
2021-04-09 10:20:45 +0800

    add scripts to run local network based dag communication: baseline
    Signed-off-by: Dong Du <ddnirvana1@gmail.com>

---
+++ src/scripts/local_ipc_callee.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## This script is supposed to run inside containers
+## It is for IPC demo case, client side
+## 					by Dong Du
+
+
+## The current path should be /env in the container
+TEST_PATH=$1
+source ./env_prepare.sh
+source ./$TEST_PATH/env_callee.sh
+node ./index_ipc_base.js
