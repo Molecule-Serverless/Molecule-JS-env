8a5ad1533e53af90404c72e7e497d2855107d27d
Lu Jun <lujun7460@csu.edu.cn>
2021-04-17 22:31:27 -0500

    tests: add bluefield dag stage test data
    Signed-off-by: Lu Jun <lujun7460@csu.edu.cn>

--- src/tests/ipc/stages/test_ipc.sh
+++ src/tests/ipc/stages/test_ipc.sh
@@ -2,12 +2,6 @@
 
 for (( i=0; i<100; i++))
 do
-	curl -v --location --request GET 'http://127.0.0.1:12301/invoke' \
-	--header 'Step-Name: frontend' \
-	--header 'App-Name: alexa_v2' \
-	--header 'Content-Type: application/json' \
-	--data-raw '{
-	    "utter" : "open smarthome to I love Taylor Swift"
-	}'
+	curl -v --location --request GET 'http://127.0.0.1:12301/invoke' --header 'Step-Name: frontend' --header 'App-Name: alexa_v2' 	--header 'Content-Type: application/json' 	--data '{ "utter" : "open smarthome to I love Taylor Swift"	}'
 done
 # Note: To see logs, add -v after curl
