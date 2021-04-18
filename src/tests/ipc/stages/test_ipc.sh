c5763e1508541c0468df2584090b4adb08eb7142
Lu Jun <lujun7460@csu.edu.cn>
2021-04-18 02:16:49 -0500

    update smartcomputer-nic data
    Signed-off-by: Lu Jun <lujun7460@csu.edu.cn>

--- src/tests/ipc/stages/test_ipc.sh
+++ src/tests/ipc/stages/test_ipc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for (( i=0; i<100; i++))
+for (( i=0; i<20; i++))
 do
 	curl -v --location --request GET 'http://127.0.0.1:12301/invoke' --header 'Step-Name: frontend' --header 'App-Name: alexa_v2' 	--header 'Content-Type: application/json' 	--data '{ "utter" : "open smarthome to I love Taylor Swift"	}'
+	sleep 1
 done
 # Note: To see logs, add -v after curl
