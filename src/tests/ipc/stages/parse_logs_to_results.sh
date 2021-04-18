a363ffcc9095bc4ab2b915359ff1da6bf851fd5f
DongDu <ddnirvana1@gmail.com>
2021-04-18 16:38:11 +0800

    rename result files
    Signed-off-by: DongDu <ddnirvana1@gmail.com>

--- src/tests/ipc/stages/parse_logs_to_results.sh
+++ src/tests/ipc/stages/parse_logs_to_results.sh
@@ -8,9 +8,11 @@ DUMP_FILE=$2
 function parse_logs(){
 	echo "Test-case: $1"
 	echo "Callee results:"
-	cat $LOG_DIR/$1-callee_logs.txt |  sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" |  grep "exe (handler)" | awk '{print $5}'
+	#cat $LOG_DIR/$1-callee_logs.txt |  sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" |  grep "exe (handler)" | awk '{print $5}'
+	cat $LOG_DIR/$1-callee_logs.txt |  sed "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" |  grep "exe (handler)" | awk '{print $5}'
 	echo "Caller results:"
-	cat $LOG_DIR/$1-caller_logs.txt | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | grep "callee comm" | awk '{print $9}'
+	#cat $LOG_DIR/$1-caller_logs.txt | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | grep "callee comm" | awk '{print $9}'
+	cat $LOG_DIR/$1-caller_logs.txt | sed "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" | grep "callee comm" | awk '{print $9}'
 	echo ""
 }
 
