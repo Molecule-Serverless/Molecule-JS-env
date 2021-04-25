dc2f0747199654d3f8da7c05d1e9d40c814d9a32
Dong Du <ddnirvana1@gmail.com>
2021-04-25 14:59:41 +0800

    update test scripts
    Signed-off-by: Dong Du <ddnirvana1@gmail.com>

--- src/tests/ipc/stages/run_alexa_stage_tests.sh
+++ src/tests/ipc/stages/run_alexa_stage_tests.sh
@@ -22,6 +22,61 @@ function run_test(){
 	docker stop $(docker ps -aq)
 }
 
+
+MOLECULE_ENV_HOME=$(pwd)/../../../../
+
+# Accept 1 args: e.g., front-interact (i.e., the name of test case)
+function run_test_r(){
+	#NAME=interact-smarthome
+	#./docker_run-IPC-client.sh tests/ipc/stages/$NAME/
+	#./docker_run-IPC-client.sh tests/ipc/stages/$1/ $1-caller
+	docker stop test_ipc_caller
+	docker run --rm --name test_ipc_caller --ipc=host -d -it -p 12301:40041 -v /tmp/fifo_dir/:/tmp/fifo_dir -v $MOLECULE_ENV_HOME/../molecule-benchmarks/:/home -v $MOLECULE_ENV_HOME/src/:/env -w /env --entrypoint=/env/scripts/local_ipc_caller.sh ddnirvana/molecule-js-env:v3-node14.16.0 tests/ipc/stages/$1/
+	sleep 1
+}
+
+# Accept 1 args: e.g., front-interact (i.e., the name of test case)
+function run_test_e(){
+	docker stop test_ipc_callee
+	#./docker_run-IPC-server.sh tests/ipc/stages/$NAME/
+	#./docker_run-IPC-server.sh tests/ipc/stages/$1/ $1-callee 12302
+	docker run --rm --name test_ipc_callee --ipc=host -d -it -p 12302:40041 -v /tmp/fifo_dir/:/tmp/fifo_dir -v $MOLECULE_ENV_HOME/../molecule-benchmarks/:/home -v $MOLECULE_ENV_HOME/src/:/env -w /env --entrypoint=/env/scripts/local_ipc_callee.sh ddnirvana/molecule-js-env:v3-node14.16.0 tests/ipc/stages/$1
+	sleep 1
+}
+
+# Accept 1 args: e.g., front-interact (i.e., the name of test case)
+function run_test_R(){
+	#NAME=interact-smarthome
+	#./docker_run-IPC-client.sh tests/ipc/stages/$NAME/
+	#./docker_run-IPC-client.sh tests/ipc/stages/$1/ $1-caller
+	docker stop test_ipc_caller
+	docker run --rm --name test_ipc_caller --ipc=host -it -p 12301:40041 -v /tmp/fifo_dir/:/tmp/fifo_dir -v $MOLECULE_ENV_HOME/../molecule-benchmarks/:/home -v $MOLECULE_ENV_HOME/src/:/env -w /env --entrypoint=/env/scripts/local_ipc_caller.sh ddnirvana/molecule-js-env:v3-node14.16.0 tests/ipc/stages/$1/
+	sleep 1
+}
+
+# Accept 1 args: e.g., front-interact (i.e., the name of test case)
+function run_test_E(){
+	docker stop test_ipc_callee
+	#./docker_run-IPC-server.sh tests/ipc/stages/$NAME/
+	#./docker_run-IPC-server.sh tests/ipc/stages/$1/ $1-callee 12302
+	docker run --rm --name test_ipc_callee --ipc=host -it -p 12302:40041 -v /tmp/fifo_dir/:/tmp/fifo_dir -v $MOLECULE_ENV_HOME/../molecule-benchmarks/:/home -v $MOLECULE_ENV_HOME/src/:/env -w /env --entrypoint=/env/scripts/local_ipc_callee.sh ddnirvana/molecule-js-env:v3-node14.16.0 tests/ipc/stages/$1
+	sleep 1
+}
+
+# 1 args: the testcase name
+function run_test_invoke(){
+	./test_ipc.sh
+	sleep 2
+	#docker logs ipc_stage_test_caller > $NAME-caller_logs.txt
+	#docker logs ipc_stage_test_callee > $NAME-callee_logs.txt
+	docker logs test_ipc_caller | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $1-caller_logs.txt
+	docker logs test_ipc_callee | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $1-callee_logs.txt
+
+	##Clean
+	docker stop test_ipc_caller
+	docker stop test_ipc_callee
+}
+
 function run_all(){
 	##Clean
 	docker stop $(docker ps -aq)
@@ -50,9 +105,12 @@ function print_usage(){
 	echo "-h: Print the help info"
 	echo "-r: run a test case's caller, e.g., -r front-interact"
 	echo "-e: run a test case's callee, e.g., -e front-interact"
+	echo "-R: run a test case's caller (show terminal/blocking), e.g., -r front-interact"
+	echo "-E: run a test case's callee (show terminal/blocking), e.g., -e front-interact"
+	echo "-i: invoke a test case (need prepared caller and calee), e.g., -e front-interact"
 }
 
-while getopts ":habc:" opt; do
+while getopts ":habc:R:E:r:e:i:" opt; do
 	case $opt in
 		a)
 			echo "Run all test cases"
@@ -64,7 +122,37 @@ while getopts ":habc:" opt; do
 			exit
 			;;
 
-
+		R)
+			run_test_R $OPTARG
+			exit
+			;;
+		E)
+			run_test_E $OPTARG
+			exit
+			;;
+		r)
+			run_test_r $OPTARG
+			exit
+			;;
+		e)
+			run_test_e $OPTARG
+			exit
+			;;
+		i)
+			run_test_invoke $OPTARG
+			exit
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument".
+			exit
+			;;
+		?)
+			echo "Invalid option: -$OPTARG index:$OPTIND"
+			exit
+			;;
 	esac
 done
+
+# In case no args passed
+print_usage
 
