cd2882f0e15e8fc7959e4b79b43a4473e252509a
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-11-22 21:54:51 +0800

    scripts: refine chained application's scripts
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- src/index_heteroIPC.js
+++ src/index_heteroIPC.js
@@ -294,7 +294,7 @@ async function main() {
         {
             msg = addon.IPCRecv(self_fifo)
             after_recving_time = get_microsecond()
-            if(msg != null)
+            if(msg != "")
             {
                 msg = JSON.parse(msg)
             }
@@ -331,7 +331,7 @@ async function main() {
             }
             else if(msg.source === 'next')
             {
-                addon.IPCSend(prev_fifo, msg)
+                addon.IPCSend(prev_fifo, JSON.stringify(msg))
             }
             else
             {
---
+++ src/tests/ipc/chain/smarthome-door/env_ipc_caller.sh
@@ -0,0 +1,7 @@
+export CODE_PATH=/home/src/smarthome/index.js
+export INFO_HOST=172.17.0.1
+export INFO_PORT=12302
+#export NEXTSTEPNAME=interact
+export FUNC_NAME=smarthome
+export IPCTest=IPCTestCaller
+export CONFIG_PATH="/env/tests/ipc/chain/smarthome-door/caller_config.json"
