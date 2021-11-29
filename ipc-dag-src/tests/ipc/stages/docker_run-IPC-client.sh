7e856be679d2aee5aea3f61b69799f7018364887
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-11-29 23:21:01 +0800

    update staged tests configs
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- ipc-dag-src/tests/ipc/stages/docker_run-IPC-client.sh
+++ ipc-dag-src/tests/ipc/stages/docker_run-IPC-client.sh
@@ -8,10 +8,10 @@
 MOLECULE_ENV_HOME=$(pwd)/../../../../
 NAME=$2
 ### To run in a bash shell, uncomment the following command
-docker run --rm --name $NAME -it -d -p 12301:40041 -v $MOLECULE_ENV_HOME/../molecule-benchmarks/:/home -v $MOLECULE_ENV_HOME/src/:/env -w /env --entrypoint=/env/scripts/local_ipc_caller.sh ddnirvana/molecule-js-env:v3-node14.16.0 $1
+docker run --rm --name $NAME -d -p 12301:40041 -v $MOLECULE_ENV_HOME/../molecule-benchmarks/:/home -v $MOLECULE_ENV_HOME/ipc-dag-src/:/env -w /env --entrypoint=/env/scripts/local_ipc_caller.sh ddnirvana/molecule-js-env:v3-node14.16.0 $1
 
 #tests/ipc/stages/interact-smarthome/
 
 ### To run in a bash shell, uncomment the following command
-#docker run --rm -it -p 12301:40041 -v $(pwd)/../../molecule-benchmarks/:/home -v $(pwd)/../../molecule-js-env/src/:/env --entrypoint=/bin/bash ddnirvana/molecule-js-env:v3-node14.16.0
+#docker run --rm -it -p 12301:40041 -v $(pwd)/../../molecule-benchmarks/:/home -v $(pwd)/../../molecule-js-env/ipc-dag-src/:/env --entrypoint=/bin/bash ddnirvana/molecule-js-env:v3-node14.16.0
 
